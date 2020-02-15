083ad035a7e574427d9986def688ec51b8d185cd
Natnael Sisay <natyjava8@gmail.com>
2020-02-15 13:31:09 +0300

    basic layout was finished

--- jCon/client/src/App.vue
+++ jCon/client/src/App.vue
@@ -1,60 +1,72 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app dark v-model="drawer">
+      <v-list dense nav class="py-0">
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+          </v-list-item-avatar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+          <v-list-item-content>
+            <v-list-item-title>Java</v-list-item-title>
+            <v-list-item-subtitle>0949632242</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- end of navigation drawer -->
+
+    <v-app-bar app dark color="green">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>CONTACT</v-toolbar-title>
 
       <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-md-and-down">
+        <v-btn text color="">Signup</v-btn>
+        <v-btn text>Login</v-btn>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <!-- if logged in user -->
+        <v-btn text>Logout</v-btn>
+      </v-toolbar-items>
+
+      <!-- mobile -->
+      <v-menu class="hidden-md-and-up">
+        <!-- <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+        <v-icon>home</v-icon>
+
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-content>
+              <p>Content</p>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">info</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list> -->
+
+        <v-btn>button</v-btn>
+      </v-menu>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <router-view></router-view>
+        <!-- <h1 class="text-center">welcome</h1> -->
+      </v-container>
     </v-content>
+    <v-footer app></v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  name: "App",
+  data() {
+    return {
+      drawer: false // Hide mobile side menu by default
+    };
+  }
 };
 </script>
--- jCon/client/src/main.js
+++ jCon/client/src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "@/router/index";
 
-Vue.config.productionTip = false
+Vue.use(router);
+// import "vuetify/dist/vuetify.min.css";
+Vue.use(vuetify);
+// Vue.use(Vuetify);
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
