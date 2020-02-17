9b372f9966b6c8c0d4c3071e59474255afb9d711
Natnael Sisay <natyjava8@gmail.com>
2020-02-17 15:50:36 +0300

    don't add vee-validate in side main
    i have added vee-validate in side main.js by calling
    vue.use(vee-validate) , and when in run the program letter, it created
    a problem saying ' can't install , or some sort of error like that
    and the root course was me calling vee-validate in there.

--- jCon/client/src/main.js
+++ jCon/client/src/main.js
@@ -3,11 +3,11 @@ import App from "./App.vue";
 
 import vuetify from "./plugins/vuetify";
 import router from "@/router/index";
-import VeeValidate from "vee-validate";
+// import VeeValidate from "vee-validate";
 
 Vue.use(router);
 Vue.use(vuetify);
-Vue.use(VeeValidate);
+// Vue.use(VeeValidate);
 
 Vue.config.productionTip = false;
 
